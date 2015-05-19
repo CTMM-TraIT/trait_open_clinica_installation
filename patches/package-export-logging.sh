7f6e620bcf987e6a56b3c6a4cb874f5a230d24cb
jacobrousseau <j.rousseau@vumc.nl>
2015-05-19 14:00:30 +0200

    Addition of logging of the access to the export files. Made the ExportLogger class static

---
+++ patches/package-export-logging.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------------------------------------
+#
+# Script to package the custom code changes to OpenClinica for the export logging. The code is
+# added to the deployed OpenClinica WAR file; this file is called for example: 
+# "OpenClinica-3.3-ExportLogging.war"
+# The script requires 2 parameters:
+#  1) the directory where the modified OpenClinica code is contained
+#  2) the tomcat home directory
+# for example '/home/johndoe/workspace-oc/OpenClinica'
+# ---------------------------------------------------------------------------------------------------------
+
+CURRENT_DIR=`pwd`
+BASE_WAR_FILE_NAME="OpenClinica-3.3";
+ORIGINAL_WAR_FILE=$BASE_WAR_FILE_NAME.war;
+TARGET_WAR_FILE=$BASE_WAR_FILE_NAME-ExportLogging.war;
+PROJECT_BASE_DIR=$1;
+TOMCAT_HOME_DIR=$2;
+
+if [ x"${PROJECT_BASE_DIR}" = x ]; then
+	echo "OpenClinica source directory not set"
+	exit 1
+fi
+
+if [ x"${TOMCAT_HOME_DIR}" = x ]; then
+	echo "Tomcat home directory not set"
+	exit 2
+fi
+
+echo "Adding custom TraIT changes for the logging of exports to the WAR: $ORIGINAL_WAR_FILE Adding to: $TARGET_WAR_FILE"; 
+echo
+echo "Source directory is $PROJECT_BASE_DIR"
+echo "Tomcat home directory is $TOMCAT_HOME_DIR" 
+
+
+
+# unpack the modified java source files and copy them to the correct location under the OpenClinica source
+tar -zcvf ExportLogging.tar.gz $PROJECT_BASE_DIR/core/src/main/java/org/akaza/openclinica/job/ExportLogger.java $PROJECT_BASE_DIR/core/src/main/java/org/akaza/openclinica/job/XsltTransformJob.java $PROJECT_BASE_DIR/web/src/main/java/org/akaza/openclinica/control/extract/AccessFileServlet.java
+
+# now build OpenClinica
+cd $PROJECT_BASE_DIR
+mvn clean install -DskipTests=true
+cd $CURRENT_DIR
+
+
+mkdir ~/package-export-logging
+cp ../logging/logback-test.xml ~/package-export-logging
+cd ~/package-export-logging
+cp $TOMCAT_HOME_DIR/webapps/OpenClinica.war .
+
+CORE_JAR_FILE_NAME=`jar -tvf OpenClinica.war | grep OpenClinica-core | grep jar | awk '{print $8}'`
+echo "Core jar file name: $CORE_JAR_FILE_NAME"
+jar -xvf OpenClinica.war $CORE_JAR_FILE_NAME
+
+mkdir -p ./org/akaza/openclinica/job/
+cp $PROJECT_BASE_DIR/core/target/classes/org/akaza/openclinica/job/ExportLogger.class ./org/akaza/openclinica/job
+cp $PROJECT_BASE_DIR/core/target/classes/org/akaza/openclinica/job/XsltTransformJob.class ./org/akaza/openclinica/job
+jar -uf $CORE_JAR_FILE_NAME ./org/akaza/openclinica/job/ExportLogger.class
+jar -uf $CORE_JAR_FILE_NAME ./org/akaza/openclinica/job/XsltTransformJob.class
+
+
+jar -uf $CORE_JAR_FILE_NAME ./logback-test.xml
+
+
+# update the JAR file in the WAR
+jar -uf ./OpenClinica.war $CORE_JAR_FILE_NAME
+
+mkdir -p WEB-INF/classes/org/akaza/openclinica/control/extract
+
+cp $PROJECT_BASE_DIR/web/target/classes/org/akaza/openclinica/control/extract/AccessFileServlet.class ./WEB-INF/classes/org/akaza/openclinica/control/extract/
+jar -uf OpenClinica.war ./WEB-INF/classes/org/akaza/openclinica/control/extract/AccessFileServlet.class
+
+# the file name OpenClinica-ExportLogging.war is expected by the deploy-export-logging.sh
+mv ./OpenClinica.war ./OpenClinica-ExportLogging.war
+rm -rf ./org
+rm -rf ./WEB-INF
+cd $CURRENT_DIR
+
+echo "Finished"
